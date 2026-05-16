8aa0f5316988ae6df76a7c5c24f92e65ad226f9d
Malik <landorjan238@gmail.com>
2026-05-15 21:19:29 -0500

    Fix formatting of JSON object in elos.js

--- elos.js
+++ elos.js
@@ -1,7 +1,7 @@
 {
-  "_content": "PLEASE DO NOT EDIT/FORK THIS FILE AS IT WILL BREAK"
-  "_version":  "v1.0"
-  "_engine": "Mivora Engine"
+  "_content": "PLEASE DO NOT EDIT/FORK THIS FILE AS IT WILL BREAK",
+  "_version":  "v1.0",
+  "_engine": "Mivora Engine",
 
   "LTS-SUPPORT": 2026-05-15
 }
